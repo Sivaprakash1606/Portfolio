06dff276d4e37dd40c48621f43e81595a06cc8d2
Sivaprakash1606 <sivasnprakash3@gmail.com>
2024-04-01 18:38:30 +0530

    successfully completed experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import About from './components/About';
+import Experience from './components/Experience';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
+import Portfolio from './components/Portfolio';
 import SocialLinks from './components/SocialLinks';
 import './index.css';
 function App() {
@@ -10,6 +13,10 @@ function App() {
         <NavBar/>
         <Home/>
         <SocialLinks/>
+        <About/>
+        <Portfolio/>
+        <Experience/>
+        
       </div>
     </>
   );
